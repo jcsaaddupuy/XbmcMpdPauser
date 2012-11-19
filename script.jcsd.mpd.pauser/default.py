931f67c76ee919f1cf203c2333b7cb112ee75417
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2012-11-20 00:16:23 +0100

    Removed debug code

--- script.jcsd.mpd.pauser/default.py
+++ script.jcsd.mpd.pauser/default.py
@@ -1,5 +1,3 @@
-from debug import Debug
-
 import sys, os
 import xbmcaddon, xbmc
 
@@ -11,7 +9,7 @@ __addon__ = xbmcaddon.Addon(id=__scriptid__)
 sys.path.append(os.path.join (__addon__.getAddonInfo('path'), 'resources', 'lib'))
 
 xbmc.log(msg="[MPD PAUSER] %s started" % (__scriptid__), level=xbmc.LOGERROR)
-Debug.launch_remote_debug()
+
 
 from notification_handler import MpdNotificationHandler
 from notification_service import NotificationService
